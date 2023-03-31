d3ac53086b97697cddbaf1b3899828503a6f54ba
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2023-03-31 09:31:38 +0200

    Drafts MTL classes

--- src/python/src/mtln.py
+++ src/python/src/mtln.py
@@ -1,30 +1,65 @@
 import numpy as np
 
-class MTLN:
-    """
-    Multiconductor Transmission Line Network.
-    """
-    number_of_ports = 0
+class Probe:
+    def __init__(self):
+        return
 
-
-class MulticonductorMedia:
+class MTL:
     """
-    Multiconductor 
+    Lossless Multiconductor Transmission Line
     """
 
-def __init__(self, L: np.array, C: np.array):
+def __init__(self, L, C, length=1.0, nx=100, Zs=0.0, Zl=0.0):
+    self.x = np.linspace(0, length, nx+1)
+    self.l = L
+    self.c = C
+    
+    self.number_of_conductors = np.shape(self.l)[0]
+    self.v = np.zeros([self.number_of_conductors, self.x.shape[0]  ])
+    self.i = np.zeros([self.number_of_conductors, self.x.shape[0]-1])
+
+    self.zs = Zs * np.eye(self.number_of_conductors)
+    self.zl = Zl * np.eye(self.number_of_conductors)
+
+def get_phase_velocities(self):
+    return 1/np.sqrt(np.diag(self.l)*np.diag(self.c))
+
+def get_max_timestep(self):
+    dx = np.min(self.x[1:] - self.x[:-1])
+    return dx / np.max(self.get_phase_velocities())
+
+def step():
+
+    
+    
+
+    cInv = np.linalg.inv(c)
+    lInv = np.linalg.inv(l)
+
+    rSc = np.matmul(rs,c)
+    rLc = np.matmul(rl,c)
+
+    sourceEq = dZ*rSc/dT-np.identity(dim)
+    loadEq   = dZ*rLc/dT-np.identity(dim)
+    sourceEqInv = np.linalg.inv(dZ*rSc/dT+np.identity(dim))
+    loadEqInv = np.linalg.inv(dZ*rLc/dT+np.identity(dim))
+
+    for _ in range(1, tSteps + 1):
+
+        v[:,0] = np.matmul(sourceEqInv,np.matmul(sourceEq,v[:,0]) - 2*np.matmul(rs,i[:,0]) + (2*vs))
+        
+        for k in range(1, zSteps):
+            
+            v[:,k] = v[:,k] - (dT/dZ)*np.matmul(cInv,i[:,k]-i[:,k-1])
+        
+        v[:,zSteps] = np.matmul(loadEqInv,np.matmul(loadEq,v[:,zSteps]) + 2*np.matmul(rl,i[:,zSteps-1]) + (2*vl))
+
+        for k in range(zSteps):
+            i[:,k] = i[:,k] - (dT/dZ)*np.matmul(lInv,v[:,k+1]-v[:,k])
+
+def add_voltage_source(self, position: float, conductor: int, magnitude):
     return
 
-def voltage_source() -> MTLN:
+def add_voltage_probe(self, position: float):
     return
 
-def line() -> MTLN:
-    return
-
-def load() -> MTLN:
-    return
-
-def short() -> MTLN:
-    return
-
-def junction() -> MTLN:
--- src/python/test/mtln_test.py
+++ src/python/test/mtln_test.py
@@ -1,35 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy.constants import epsilon_0, mu_0
+import scipy as sci
+from scipy.constants import epsilon_0, mu_0, speed_of_light
 
-import skrf as rf
+import src.mtln as mtln
 
-def test_skrf():
-    Z_0 = 50
-    Z_L = 75
-    theta = 0
+def gaussian(x, x0, s0):
+    return np.exp( - (x-x0)**2 / (2*s0**2) )
 
-    # the necessary Frequency description
-    freq = rf.Frequency(start=1, stop=2, unit='GHz', npoints=3)
+def square_pulse(x, A, x0):
+    return A*(x <= x0)*(x >= 0)
 
-    # The combination of a transmission line + a load can be created
-    # using the convenience delay_load method
-    # important: all the Network must have the parameter "name" defined
-    tline_media = rf.DefinedGammaZ0(freq, z0=Z_0)
-    delay_load = tline_media.delay_load(rf.zl_2_Gamma0(Z_0, Z_L), theta, unit='deg', name='delay_load')
 
-    # the input port of the circuit is defined with the Circuit.Port method
-    port1 = rf.Circuit.Port(freq, 'port1', z0=Z_0)
 
-    # connexion list
-    cnx = [
-        [(port1, 0), (delay_load, 0)]
-    ]
-    # building the circuit
-    cir = rf.Circuit(cnx)
+def test_get_phase_velocities():
+    v = mtln.MTL(l=0.25e-6, c=100e-12).get_phase_velocities()
+    assert v.shape == (1,)
+    assert np.isclose((2/3)*speed_of_light, v[0])
 
-    # getting the resulting Network from the 'network' parameter:
-    ntw = cir.network
-    print(ntw)
-    print("END")
+def test_coaxial_line_initial_voltage():
+    line = mtln.MTL(l=0.25e-6, c= 100e-12, length=400)
+    line.set_voltage(lambda x: gaussian(x, 200, 50))
+
+def test_coaxial_line_paul_8_6():
+    """ 
+    Described in Ch. 8 of Paul Clayton, 
+    Analysis of Multiconductor Transmission Lines. 2004. 
+    """
+    
+    line = mtln.MTL(l=0.25e-6, c= 100e-12, length=400.0, Zs = 150)
+    finalTime = 18e-6
+    tRange = np.arange(0, np.floor(finalTime / line.get_timestep()))
+
+    magnitude = lambda t: square_pulse(t, 100, 6e-6)
+    line.add_voltage_source(magnitude, position=0.0)
+    voltage_probe = line.add_voltage_probe(position=0.0)
+    
+    for t in tRange:
+        line.step()
+
+    plt.plot(voltage_probe.t, voltage_probe.v)
+    plt.sho()
+
+
+
