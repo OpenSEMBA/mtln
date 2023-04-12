Fixes bug in time step computation Adds multiconductor example, ribbon cable, 9.3.1 in Paul 2007

src/python/src/mtln.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Probe:
-    def __init__(self, position, conductor = 0, type = 'voltage'):
+    def __init__(self, position, conductor, type):
         self.type = type
         self.position = position
         self.conductor = conductor
@@ -22,7 +22,7 @@ class MTL:
         if type(l) == float and type(c) == float:
             self.l = np.array([[l]])
             self.c = np.array([[c]])
-        elif type(l) == np.array and type(c) == np.array:
+        elif type(l) == np.ndarray and type(c) == np.ndarray:
             assert(l.shape == c.shape)
             assert(l.shape[0] == l.shape[1])
             self.l = l
@@ -32,10 +32,10 @@ class MTL:
         
         self.number_of_conductors = np.shape(self.l)[0]
         
-        if (type(Zs) == float and type(Zs) == float):
+        if (type(Zs) != np.ndarray and type(Zl) != np.ndarray):
             self.zs = Zs * np.eye(self.number_of_conductors)
             self.zl = Zl * np.eye(self.number_of_conductors)
-        elif (type(Zs) == np.array and type(Zs) == np.array):
+        elif (type(Zs) == np.ndarray and type(Zl) == np.ndarray):
             assert(Zs.shape == Zl.shape)
             if (len(Zs.shape) == 1):
                 assert(Zs.shape[0] == self.number_of_conductors)
@@ -50,6 +50,8 @@ class MTL:
         else:
             raise ValueError("Invalid input Zs and/or Zc. Use two floats or two arrays")
                
+              
+            
         
         self.v = np.zeros([self.number_of_conductors, self.x.shape[0]  ])
         self.i = np.zeros([self.number_of_conductors, self.x.shape[0]-1])
@@ -83,8 +85,8 @@ class MTL:
 
 
     def get_phase_velocities(self):
-        return 1/np.sqrt(np.diag(self.l)*np.diag(self.c))
-
+        # return 1/np.sqrt(np.diag(self.l)*np.diag(self.c))
+        return 1/np.sqrt(np.diag(self.l.dot(self.c)))
     def get_max_timestep(self):
         dx = self.x[1] - self.x[0]
         return dx / np.max(self.get_phase_velocities())
@@ -138,15 +140,9 @@ class MTL:
         self.v_sources[conductor, index] = magnitude
         return
 
-    def add_voltage_probe(self, position: float):
+    def add_probe(self, position: float, conductor: int, type: str):
         
-        voltage_probe = Probe(position)
-        self.probes.append(voltage_probe)        
-        return voltage_probe
-
-    def add_current_probe(self, position: float):
-        
-        current_probe = Probe(position, type = 'current')
-        self.probes.append(current_probe)        
-        return current_probe
+        probe = Probe(position, conductor, type)
+        self.probes.append(probe)        
+        return probe
 

src/python/test/mtln_test.py
@@ -17,7 +17,34 @@ def square_pulse(x, A, x0):
 def triangle_pulse(x, A, x0):
     return A*(x/x0)*(x <= x0)*(x >= 0)
 
+def trapezoidal_pulse(x, A, rise_time, fall_time, f0, D):
+    mod_time = x % (1/f0)
+    cte_time = D/f0 - 0.5 * (rise_time + fall_time)
+    
+    t1, t2, t3 = rise_time, rise_time + cte_time, rise_time + cte_time + fall_time
+    
+    return A*(mod_time/rise_time)*(mod_time <= t1) + \
+           A*(mod_time >= t1 )*(mod_time <= t2) + \
+           (-A*mod_time/fall_time + A*(rise_time + cte_time + fall_time)/fall_time)*(mod_time >= t2)*((mod_time <= t3))
+    
 
+def test_trapezoidal_pulse():
+    A = 1
+    rise_time = 10e-9
+    fall_time = 10e-9
+    f0 = 20e6
+    D = 0.5
+    # magnitude = lambda t: trapezoidal_pulse(t, A, rise_time, fall_time, f0, D)
+    plateu_duration = D/f0 - 0.5 * (rise_time + fall_time)
+
+    time = np.linspace(0,200e-9,1000)
+    magnitude = lambda t: trapezoidal_pulse(t, A = 1, rise_time=20e-9, fall_time=20e-9, f0=1e6, D=0.5)
+    plt.plot(time, magnitude(time))
+    plt.show()
+    # assert (magnitude(0.5*rise_time) == 0.5*A)
+    # assert (magnitude(1.1*rise_time) == A)
+    # assert (magnitude(rise_time + plateu_duration + 0.5*fall_time) == 0.5*A)
+    
 
 def test_get_phase_velocities():
     v = mtln.MTL(l=0.25e-6, c=100e-12).get_phase_velocities()
@@ -49,8 +76,8 @@ def test_coaxial_line_paul_8_6_square():
 
     magnitude = lambda t: square_pulse(t, 100, 6e-6)
     line.add_voltage_source(position=0.0, conductor=0, magnitude=magnitude)
-    voltage_probe = line.add_voltage_probe(position=0.0)
-    current_probe = line.add_current_probe(position=400.0)
+    voltage_probe = line.add_probe(position=0.0, conductor=0, type='voltage')
+    current_probe = line.add_probe(position=400.0,conductor=0, type='current')
     
     for t in tRange:
         line.step()
@@ -84,13 +111,13 @@ def test_coaxial_line_paul_8_6_triangle():
     Analysis of Multiconductor Transmission Lines. 2004. 
     """
     
-    line = mtln.MTL(l=0.25e-6, c= 100e-12, length=400.0, Zs = 150)
+    line = mtln.MTL(l=0.25e-6, c= 100e-12, length=400.0, Zs = 150.0)
     finalTime = 18e-6
     tRange = np.arange(0, np.floor(finalTime / line.get_max_timestep()))
 
     magnitude = lambda t: triangle_pulse(t, 100, 6e-6)
     line.add_voltage_source(position=0.0, conductor=0, magnitude=magnitude)
-    voltage_probe = line.add_voltage_probe(position=0.0)
+    voltage_probe = line.add_probe(position=0.0, conductor=0, type='voltage')
     
     for t in tRange:
         line.step()
@@ -113,23 +140,34 @@ def test_coaxial_line_paul_8_6_triangle():
 
 def test_ribbon_cable_paul_9_3():
     """
-    Described in Ch. 9 of Paul Clayton
+    Described in Ch. 9.3.1 "Ribbon Cables" of Paul Clayton
     Analysis of Multiconductor Transmission Lines. 2007. 
     """
-    l = np.array(shape=(2,2))
+    l = np.zeros([2,2])
     l[0] = [0.7485*1e-6 ,0.5077*1e-6]
     l[1] = [0.5077*1e-6, 1.0154*1e-6]
-    c = np.array(shape=(2,2))
+    c = np.zeros([2,2])
     c[0] = [37.432*1e-12, -18.716*1e-12]
     c[1] = [-18.716*1e-12, 24.982*1e-12]
 
-    line = mtln.MTL(l=l, c= c, length=2.0, Zs = 150)
-    finalTime = 18e-6
+    Zs, Zl = np.zeros([1,2]), np.zeros([1,2])
+    Zs[:] = [50,50]
+    Zl[:] = [50,50]
+
+    line = mtln.MTL(l=l, c= c, length=2.0, nx = 2, Zs = Zs, Zl = Zl)
+    finalTime = 200e-9
     tRange = np.arange(0, np.floor(finalTime / line.get_max_timestep()))
 
-    magnitude = lambda t: triangle_pulse(t, 100, 6e-6)
-    line.add_voltage_source(position=0.0, conductor=0, magnitude=magnitude)
-    voltage_probe = line.add_voltage_probe(position=0.0)
+    magnitude = lambda t: trapezoidal_pulse(t, A = 1, rise_time=20e-9, fall_time=20e-9, f0=1e6, D=0.5)
+    line.add_voltage_source(position=0.0, conductor=1, magnitude=magnitude)
+    voltage_probe = line.add_probe(position=0.0, conductor= 0, type='voltage')
     
     for t in tRange:
         line.step()
+
+    plt.plot(1e9*voltage_probe.t, 1e3*voltage_probe.v)
+    plt.ylabel(r'$V_1 (0, t)\,[mV]$')
+    plt.xlabel(r'$t\,[\mu s]$')
+    plt.xticks(range(0, 200 ,50))
+    plt.grid('both')
+    plt.show()