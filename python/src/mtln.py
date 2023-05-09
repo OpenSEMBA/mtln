fcacceb872048871b9853aebdcb7afb671f0b657
alberto <albergascon@gmail.com>
2023-05-09 08:32:55 +0200

    Adds draft for mtln class

---
+++ python/src/mtln.py
@@ -0,0 +1,17 @@
+import numpy as np
+import skrf as rf
+
+from copy import deepcopy
+from numpy.fft import fft, fftfreq, fftshift
+import sympy as sp
+
+from types import FunctionType
+from types import LambdaType
+
+class MTLN:
+    """
+    Lossless Multiconductor Transmission Line Network 
+    """
+
+    def __init__(self):
+        pass
