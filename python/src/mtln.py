033437f2c7cbb80b8cb5394d719aa58ab9f3d12a
alberto <albergascon@gmail.com>
2023-05-17 13:22:37 +0200

    Drafting definitions and structure of networks

--- python/src/mtln.py
+++ python/src/mtln.py
@@ -5,13 +5,38 @@ from copy import deepcopy
 from numpy.fft import fft, fftfreq, fftshift
 import sympy as sp
 
+import src.mlt as mtl
+
 from types import FunctionType
 from types import LambdaType
+
+
+# class Node:
+#     def __init__(self, network: Network):
+#         self.connections = {}
+
+class Network:
+    """
+    Networks can be joining tubes (junctions) or ending tubes (terminations)
+    """
+    def __init__(self, nodes: list[int]):
+        self.number_of_nodes = len(nodes)
+        self.connections = {}
+    
+    def add_connection(self,nw_node, tube:mtl.mtl, conductor: int):
+        self.connections[nw_node] = [mtl,conductor]
+        
+
+
+        
 
 class MTLN:
     """
     Lossless Multiconductor Transmission Line Network 
     """
+    def __init__(self, l, c, g, r, length=1.0, nx=100, Zs=0.0, Zl=0.0):
+        super().__init__(l, c, length=length, nx=nx, Zs=Zs, Zl=Zl)
 
-    def __init__(self):
-        pass
+
+    #the step function has to be redefined, to be compatible with the
+    #network structure and update
--- python/test/mtln_test.py
+++ python/test/mtln_test.py
@@ -4,55 +4,64 @@ import matplotlib.pyplot as plt
 import sympy as sp
 from scipy.constants import epsilon_0, mu_0, speed_of_light
 
+import src.mtl as mtl
+import src.mtln as mtln
+
+
 import src.waveforms as wf
 
 import skrf as rf
 from skrf.media import DistributedCircuit
 
-def test_mtln_2_tubes_short():
-    """ 
-    Analogous in Ch. 8 of Paul Clayton, 
+def test_ribbon_cable_20ns_paul_9_3():
+    """
+    Described in Ch. 9.3.1 "Ribbon Cables" of Paul Clayton
     Analysis of Multiconductor Transmission Lines. 2007. 
     """
     """
-    Two mtl with half lenght, shorted at the connection point
+    Solved with mtl approach and mltn approach: tube + 2 termination networks
     """
-    # line = mtl.MTL(l=0.25e-6, c=100e-12, length=400.0, Zs=150)
-    # finalTime = 18e-6
+    l = np.zeros([2, 2])
+    l[0] = [0.7485*1e-6, 0.5077*1e-6]
+    l[1] = [0.5077*1e-6, 1.0154*1e-6]
+    c = np.zeros([2, 2])
+    c[0] = [37.432*1e-12, -18.716*1e-12]
+    c[1] = [-18.716*1e-12, 24.982*1e-12]
 
-    # def magnitude(t): return wf.square_pulse(t, 100, 6e-6)
-    # line.add_voltage_source(position=0.0, conductor=0, magnitude=magnitude)
-    # v_probe = line.add_probe(position=0.0, conductor=0, type='voltage')
-    # i_probe = line.add_probe(position=400.0, conductor=0, type='current')
+    Zs, Zl = np.zeros([1, 2]), np.zeros([1, 2])
+    Zs[:] = [50, 50]
+    Zl[:] = [50, 50]
 
-    # for t in line.get_time_range(finalTime):
-    #     line.step()
+    line = mtl.MTL(l=l, c=c, length=2.0, nx=2, Zs=Zs, Zl=Zl)
+    finalTime = 200e-9
 
-    # xticks = range(int(np.floor(min(1e6*i_probe.t))),
-    #                int(np.ceil(max(1e6*i_probe.t))+1))
+    def magnitude(t): return wf.trapezoidal_wave(
+        t, A=1, rise_time=20e-9, fall_time=20e-9, f0=1e6, D=0.5)
+    line.add_voltage_source(position=0.0, conductor=1, magnitude=magnitude)
+    v_probe = line.add_probe(position=0.0, type='voltage')
 
-    # start_times = [0.1, 4.1, 6.1, 8.1, 10.1, 12.1, 14.1, 16.1]
-    # end_times = [3.9, 5.9, 7.9, 9.9, 11.9, 13.9, 15.9, 18.9]
-    # check_voltages = [25, -12.5, -37.5, -18.75, 18.75, 9.375, -9.375, -4.6875]
-    # for (t_start, t_end, v) in zip(start_times, end_times, check_voltages):
-    #     start = np.argmin(np.abs(v_probe.t - t_start*1e-6))
-    #     end = np.argmin(np.abs(v_probe.t - t_end*1e-6))
-    #     assert np.all(np.isclose(v_probe.val[start:end], v))
+    line.run_until(finalTime)
 
-    # plt.figure()
-    # plt.plot(1e6*v_probe.t, v_probe.val)
-    # plt.ylabel(r'$V (0, t)\,[V]$')
-    # plt.xlabel(r'$t\,[\mu s]$')
-    # plt.grid('both')
+    """
+     _             _
+    | |     0     | |
+    | 0-----------2 |
+    | |           | |
+    | 1-----------3 |
+    |_|     1     |_|
+    T1             T2
+    
+    """
 
-    # plt.figure()
-    # plt.plot(1e6*i_probe.t, i_probe.val )
-    # plt.ylabel(r'$I (L, t)\,[A]$')
-    # plt.xlabel(r'$t\,[\mu s]$')
-    # plt.xticks(xticks)
-    # plt.grid('both')
+    line_nw = mtln.MTLN(l=l, c=c, length=2.0, nx=2)
+    term_1 = mtln.Network([0,1])
+    term_2 = mtln.Network([2,3])
+    term_1.add_connection(0, [line_nw,0])
+    term_1.add_connection(1, [line_nw,1])
+    term_2.add_connection(2, [line_nw,0])
+    term_2.add_connection(3, [line_nw,1])
 
-    # plt.show()
+    # From Paul's book:
+    # "The crosstalk waveform rises to a peak of around 110 mV [...]"
+    assert (np.isclose(np.max(v_probe.val[:, 0]), 113e-3, atol=1e-3))
 
-def test_mtln_2_tubes_R():
-    pass
